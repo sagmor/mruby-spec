1c4e1c551a469d43a2a2392d224b7c410fc417bd
Seba Gamboa <me@sagmor.com>
2015-09-07 11:51:30 -0300

    Fix runner spec

--- test/runner_spec.rb
+++ test/runner_spec.rb
@@ -2,14 +2,13 @@ mspec_runner_executed = false
 
 describe MSpec::Runner do
   it "executes examples like this one" do
-    expect(mspec_runner_executed).to be_false
-    mspec_runner_executed = false
+    mspec_runner_executed = true
   end
 end
 
 # Normal MRuby test assertion to ensure specs were executed
 assert('MSpec::Runner executes examples') do
   # mspec_runner_executed was set true on the example above
-  assert_true(mspec_runner_executed)
+  assert_true(mspec_runner_executed, "MSpec isn't running")
 end
 
